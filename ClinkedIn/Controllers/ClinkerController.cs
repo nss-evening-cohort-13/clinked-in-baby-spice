1e148b9624d136766d99b0230eae56d4a70405b7
Michael Pfohl <rmpfohl@gmail.com>
2021-02-22 19:34:16 -0600

    create model, repo, and controller for clinkers

---
+++ ClinkedIn/Controllers/ClinkerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn.Data;
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/Clinkers")]
+    [ApiController]
+    public class ClinkerController : ControllerBase
+    {
+        ClinkerRepository _repo;
+
+        public ClinkerController()
+        {
+            _repo = new ClinkerRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllClinkers()
+        {
+            return Ok(_repo.GetAll());
+        }
+    }
+}
---
+++ ClinkedIn/Data/ClinkerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn.Models;
+
+namespace ClinkedIn.Data
+{
+    public class ClinkerRepository
+    {
+        static List<Clinker> _clinkers = new List<Clinker>
+        {
+            new Clinker { Name = "Dingus", Id = 1, Services = { "Penguin Thievery", "Antarctic Sushi Making", "Toilet Sake Making" },  Interests = { "Home Brewing", "Romance Novels"}, DaysLeft = 666},
+            new Clinker { Name = "Pingus", Id = 2, Services = { "Penguin Thievery", "Trumpet", "Ice Fishing"}, Interests = { "Animals", "Fishing" }, DaysLeft = 420 },
+        };
+
+        public List<Clinker> GetAll()
+        {
+            return _clinkers;
+        }
+    }
+}
