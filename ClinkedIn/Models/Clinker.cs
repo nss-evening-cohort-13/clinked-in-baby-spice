72a170430d9bccc415ceabf4513fe354d7d7de85
Michael Pfohl <rmpfohl@gmail.com>
2021-02-22 20:35:56 -0600

    add friend

--- ClinkedIn/Models/Clinker.cs
+++ ClinkedIn/Models/Clinker.cs
@@ -11,8 +11,8 @@ namespace ClinkedIn.Models
         public int Id { get; set; }
         public List<string> Services { get; set; } = new List<string>();
         public List<string> Interests { get; set; } = new List<string>();
-        public List<Clinker> Friends { get; set; } = new List<Clinker>();
-        public List<Clinker> Enemies { get; set; } = new List<Clinker>();
+        public List<int> Friends { get; set; } = new List<int>();
+        public List<int> Enemies { get; set; } = new List<int>();
         public bool Lonely { get; set; }
         public int DaysLeft { get; set; }
     }
